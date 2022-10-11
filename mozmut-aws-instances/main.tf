ebc08ac2dc7f9af3bab7e5152e219c5d14be9d46
Mert Özmut <mozmut@mozmut-M1.local>
2022-10-11 10:02:01 +0300

    initial commit

---
+++ mozmut-aws-instances/main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+}
+
+resource "aws_security_group" "project-iac-sg" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.credentialprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Ping Transport
+  ingress {
+    from_port = 8
+    protocol = "icmp"
+    to_port = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "random_integer" "test" {
+  min = 1
+  max = 50000
+}
+
+resource "aws_instance" "mozmut-project-iac" {
+  count = 1
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.credentialprops, "subnet")
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.credentialprops, "keyname")
+  user_data =<<EOF
+#! /bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Deployed via Terraform, Hello World from $(hostname -f)</h1>" | sudo tee /var/www/html/index.html
+EOF
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 8
+    volume_type = "gp2"
+  }
+  tags = {
+    #Name ="mozmut-instance-${random_integer.test.result}"
+    Name = "mozmut-instance-${element(var.instance_names, count.index)}"
+    Environment = "DEV"
+    OS = "AMAZON"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
+}
+
+
+output "ec2instance" {
+  value = ["${ aws_instance.mozmut-project-iac.*.public_ip }"]
+}
---
+++ mozmut-aws-instances/variable.tf
@@ -0,0 +1,14 @@
+variable "awsprops" {
+  type = map(string)
+  default = {
+    region = "eu-west-1"
+    ami = "ami-09e2d756e7d78558d"
+    itype = "t2.micro"
+    publicip = true
+    keyname = "EC2 Tutorial"
+  }
+}
+
+variable "instance_names" {
+  default = ["1", "2", "3", "4", "5"]
+}
